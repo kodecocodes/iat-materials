86f1112ef10a20daa4fe5b145ec5da7ad346daad
richard.turton <jrturton@gmail.com>
2022-02-21 12:52:16 +0000

    Chapter 20 project updates

--- 20-uinavigationcontroller-custom-transition-animations/projects/final/LogoReveal/RevealAnimator.swift
+++ 20-uinavigationcontroller-custom-transition-animations/projects/final/LogoReveal/RevealAnimator.swift
@@ -1,29 +1,38 @@
-/*
- * Copyright (c) 2014-present Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+/// Copyright (c) 2022-present Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import UIKit
 
 class RevealAnimator: NSObject, UIViewControllerAnimatedTransitioning, CAAnimationDelegate {
-
   let animationDuration = 2.0
   var operation: UINavigationController.Operation = .push
 
@@ -36,10 +45,10 @@ class RevealAnimator: NSObject, UIViewControllerAnimatedTransitioning, CAAnimati
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
     storedContext = transitionContext
 
-    if operation == .push {
-      let fromVC = transitionContext.viewController(forKey: .from) as! MasterViewController
-      let toVC = transitionContext.viewController(forKey: .to) as! DetailViewController
-
+    if
+      operation == .push,
+      let fromVC = transitionContext.viewController(forKey: .from) as? MainViewController,
+      let toVC = transitionContext.viewController(forKey: .to) as? DetailViewController {
       transitionContext.containerView.addSubview(toVC.view)
       toVC.view.frame = transitionContext.finalFrame(for: toVC)
 
@@ -68,12 +77,12 @@ class RevealAnimator: NSObject, UIViewControllerAnimatedTransitioning, CAAnimati
   func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
     if let context = storedContext {
       context.completeTransition(!context.transitionWasCancelled)
-      //reset logo
-      let fromVC = context.viewController(forKey: .from) as! MasterViewController
-      fromVC.logo.removeAllAnimations()
+      // reset logo
+      let fromVC = context.viewController(forKey: .from) as? MainViewController
+      fromVC?.logo.removeAllAnimations()
 
-      let toVC = context.viewController(forKey: .to) as! DetailViewController
-      toVC.view.layer.mask = nil
+      let toVC = context.viewController(forKey: .to) as? DetailViewController
+      toVC?.view.layer.mask = nil
     }
     storedContext = nil
   }
