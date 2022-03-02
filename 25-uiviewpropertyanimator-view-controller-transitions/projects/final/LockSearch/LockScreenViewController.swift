6e0055b1e61f002f20d2e59222b84c78b113c115
richard.turton <jrturton@gmail.com>
2022-03-02 19:56:28 +0000

    Chapter 25 material updates

--- 25-uiviewpropertyanimator-view-controller-transitions/projects/final/LockSearch/LockScreenViewController.swift
+++ 25-uiviewpropertyanimator-view-controller-transitions/projects/final/LockSearch/LockScreenViewController.swift
@@ -1,69 +1,73 @@
-/*
- * Copyright (c) 2016-present Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+/// Copyright (c) 2022-present Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 // image by NASA: https://www.flickr.com/photos/nasacommons/29193068676/
 
 import UIKit
 
 class LockScreenViewController: UIViewController {
-
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var dateTopConstraint: NSLayoutConstraint!
 
   let blurView = UIVisualEffectView(effect: nil)
 
-  var settingsController: SettingsViewController!
-
+  var settingsController: SettingsViewController?
   var startFrame: CGRect?
   var previewView: UIView?
   var previewAnimator: UIViewPropertyAnimator?
 
   let previewEffectView = IconEffectView(blur: .extraLight)
   let presentTransition = PresentTransition()
-
   var isDragging = false
   var isPresentingSettings = false
-
   var touchesStartPointY: CGFloat?
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     let renderer = UIGraphicsImageRenderer(size: CGSize(width: 44, height: 33))
-    let image = renderer.image {
-      context in
+    let image = renderer.image { context in
       UIColor(white: 0, alpha: 0.4).setFill()
       UIBezierPath(roundedRect: context.format.bounds, cornerRadius: 10).fill()
-    }.stretchableImage(withLeftCapWidth: 11, topCapHeight: 11)
+    }
+      .stretchableImage(withLeftCapWidth: 11, topCapHeight: 11)
     searchBar.setSearchFieldBackgroundImage(image, for: .normal)
-    
     view.bringSubviewToFront(searchBar)
     blurView.isUserInteractionEnabled = false
     view.insertSubview(blurView, belowSubview: searchBar)
 
     tableView.estimatedRowHeight = 130.0
     tableView.rowHeight = UITableView.automaticDimension
-
     previewEffectView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissMenu)))
   }
 
@@ -72,41 +76,50 @@ class LockScreenViewController: UIViewController {
   }
 
   override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     tableView.transform = CGAffineTransform(scaleX: 0.67, y: 0.67)
     tableView.alpha = 0
-
     dateTopConstraint.constant -= 100
     view.layoutIfNeeded()
   }
 
   override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     AnimatorFactory.scaleUp(view: tableView)
       .startAnimation()
-    AnimatorFactory.animateConstraint(view: view, constraint: dateTopConstraint, by: 100)
+    AnimatorFactory.animateConstraint(
+      view: view,
+      constraint: dateTopConstraint,
+      by: 100)
       .startAnimation()
   }
 
   func toggleBlur(_ blurred: Bool) {
-    UIViewPropertyAnimator(duration: 0.55,
-                           controlPoint1: CGPoint(x: 0.57, y: -0.4),
-                           controlPoint2: CGPoint(x: 0.96, y: 0.87),
-                           animations: blurAnimations(blurred))
+    UIViewPropertyAnimator(
+      duration: 0.55,
+      controlPoint1: CGPoint(x: 0.57, y: -0.4),
+      controlPoint2: CGPoint(x: 0.96, y: 0.87),
+      animations: blurAnimations(blurred))
       .startAnimation()
   }
-  
+
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
   }
 
   @IBAction func presentSettings(_ sender: Any? = nil) {
-    //present the view controller
+    // present the view controller
     presentTransition.auxAnimations = blurAnimations(true)
-    settingsController = storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as? SettingsViewController
-    settingsController.transitioningDelegate = self
-    settingsController.didDismiss = { [unowned self] in
-      self.toggleBlur(false)
+    if let settings = storyboard?.instantiateViewController(
+      withIdentifier: "SettingsViewController")
+        as? SettingsViewController {
+      settingsController = settings
+      settings.transitioningDelegate = self
+      settings.didDismiss = { [unowned self] in
+        self.toggleBlur(false)
+      }
+      present(settings, animated: true, completion: nil)
     }
-    present(settingsController, animated: true, completion: nil)
   }
 
   func blurAnimations(_ blurred: Bool) -> () -> Void {
@@ -116,25 +129,66 @@ class LockScreenViewController: UIViewController {
       self.tableView.alpha = blurred ? 0.33 : 1.0
     }
   }
+
+  override func touchesBegan(
+    _ touches: Set<UITouch>,
+    with event: UIEvent?
+  ) {
+    guard presentTransition.wantsInteractiveStart == false,
+      presentTransition.animator != nil else {
+      return
+    }
+
+    touchesStartPointY = touches.first?.location(in: view).y
+    presentTransition.pause()
+  }
+
+  override func touchesMoved(
+    _ touches: Set<UITouch>,
+    with event: UIEvent?
+  ) {
+    guard
+      let startY = touchesStartPointY,
+      let currentPoint = touches.first?.location(in: view).y
+    else {
+      return
+    }
+
+    if currentPoint < startY - 40 {
+      touchesStartPointY = nil
+      presentTransition.animator?.addCompletion { _ in
+        self.blurView.effect = nil
+      }
+      presentTransition.cancel()
+    } else if currentPoint > startY + 40 {
+      touchesStartPointY = nil
+      presentTransition.finish()
+    }
+  }
 }
 
 extension LockScreenViewController: WidgetsOwnerProtocol {
   func startPreview(for forView: UIView) {
     previewView?.removeFromSuperview()
-    previewView = forView.snapshotView(afterScreenUpdates: false)
-    view.insertSubview(previewView!, aboveSubview: blurView)
-
-    previewView?.frame = forView.convert(forView.bounds, to: view)
-    startFrame = previewView?.frame
-    addEffectView(below: previewView!)
-
-    previewAnimator = AnimatorFactory.grow(view: previewEffectView, blurView: blurView)
+    if let preview = forView.snapshotView(afterScreenUpdates: false) {
+      previewView = preview
+      view.insertSubview(preview, aboveSubview: blurView)
+      preview.frame = forView.convert(forView.bounds, to: view)
+      startFrame = preview.frame
+      addEffectView(below: preview)
+      previewAnimator = AnimatorFactory.grow(
+        view: previewEffectView,
+        blurView: blurView)
+    }
   }
 
   func addEffectView(below forView: UIView) {
     previewEffectView.removeFromSuperview()
     previewEffectView.frame = forView.frame
-    forView.superview?.insertSubview(previewEffectView, belowSubview: forView)
+
+    forView.superview?.insertSubview(
+      previewEffectView,
+      belowSubview: forView)
   }
 
   func updatePreview(percent: CGFloat) {
@@ -161,17 +215,14 @@ extension LockScreenViewController: WidgetsOwnerProtocol {
     previewAnimator?.stopAnimation(false)
     previewAnimator?.finishAnimation(at: .end)
     previewAnimator = nil
-    
     AnimatorFactory.complete(view: previewEffectView)
       .startAnimation()
-
-    blurView.effect = UIBlurEffect(style: .dark)
     blurView.isUserInteractionEnabled = true
     blurView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissMenu)))
   }
 
   @objc func dismissMenu() {
-    let reset = AnimatorFactory.reset(frame: startFrame!, view: previewEffectView, blurView: blurView)
+    let reset = AnimatorFactory.reset(frame: startFrame ?? .zero, view: previewEffectView, blurView: blurView)
     reset.addCompletion { _ in
       self.previewEffectView.removeFromSuperview()
       self.previewView?.removeFromSuperview()
@@ -179,61 +230,33 @@ extension LockScreenViewController: WidgetsOwnerProtocol {
     }
     reset.startAnimation()
   }
-
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard presentTransition.wantsInteractiveStart == false,
-      presentTransition.animator != nil else {
-        return
-    }
-
-    touchesStartPointY = touches.first!.location(in: view).y
-    presentTransition.interruptTransition()
-  }
-
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let startY = touchesStartPointY else {
-      return
-    }
-
-    let currentPoint = touches.first!.location(in: view).y
-    if currentPoint < startY - 40 {
-      touchesStartPointY = nil
-      presentTransition.animator?.addCompletion { _ in
-        self.blurView.effect = nil
-      }
-      presentTransition.cancel()
-    } else if currentPoint > startY + 40 {
-      touchesStartPointY = nil
-      presentTransition.finish()
-    }
-  }
 }
 
 extension LockScreenViewController: UITableViewDataSource {
-
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 2
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     if indexPath.row == 1 {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "Footer") as! FooterCell
-      cell.didPressEdit = {[unowned self] in
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Footer", for: indexPath)
+      (cell as? FooterCell)?.didPressEdit = {[unowned self] in
         self.presentTransition.wantsInteractiveStart = false
         self.presentSettings()
       }
       return cell
     } else {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! WidgetCell
-      cell.tableView = tableView
-      cell.owner = self
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+      if let widgetCell = cell as? WidgetCell {
+        widgetCell.tableView = tableView
+        widgetCell.owner = self
+      }
       return cell
     }
   }
 }
 
 extension LockScreenViewController: UISearchBarDelegate {
-
   func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
     toggleBlur(true)
   }
@@ -254,12 +277,18 @@ extension LockScreenViewController: UISearchBarDelegate {
 }
 
 extension LockScreenViewController: UIViewControllerTransitioningDelegate {
-  func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-      return presentTransition
+  func animationController(
+    forPresented presented: UIViewController,
+    presenting: UIViewController,
+    source: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return presentTransition
   }
 
-  func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-      return presentTransition
+  func interactionControllerForPresentation(
+    using animator: UIViewControllerAnimatedTransitioning
+  ) -> UIViewControllerInteractiveTransitioning? {
+    return presentTransition
   }
 }
 
@@ -286,7 +315,11 @@ extension LockScreenViewController: UIScrollViewDelegate {
     }
   }
 
-  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+  func scrollViewWillEndDragging(
+    _ scrollView: UIScrollView,
+    withVelocity velocity: CGPoint,
+    targetContentOffset: UnsafeMutablePointer<CGPoint>
+  ) {
     let progress = max(0.0, min(1.0, ((-scrollView.contentOffset.y) - 30) / 90.0))
 
     if progress > 0.5 {
